006bdcdb7eac752bb51a39b3ff62d83f34f74751
uhbif19 <uhbif19@gmail.com>
2022-08-04 00:11:37 +0300

    Social auth and test page

--- github_viewer/urls.py
+++ github_viewer/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from viewer import views
+
 
 urlpatterns = [
+    path('', views.ViewAccount.as_view()),
     path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
 ]
