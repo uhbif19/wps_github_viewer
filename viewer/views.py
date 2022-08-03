5d4bec27ae5414f6a5090d4a438827186a064838
uhbif19 <uhbif19@gmail.com>
2022-08-04 01:07:37 +0300

    Fix unauth user case

--- viewer/views.py
+++ viewer/views.py
@@ -9,12 +9,23 @@ class ViewAccount(TemplateView):
     template_name = 'view_account.html'
 
     def get_context_data(self, **kwargs):
-        user_auth = UserSocialAuth.objects.get(user=self.request.user)
-        access_token = user_auth.extra_data['access_token']
-        client = GithubClient(access_token)
-        user_data = client.get_user_data()
-        if user_data is None:
-            repos = []
+        access_token = self._get_access_token()
+        if access_token is not None:
+            client = GithubClient(access_token)
+            user_data = client.get_user_data()
+            if user_data is None:
+                repos = []
+            else:
+                repos = client.get_repos(user_data)
+            return dict(user_data=user_data, repos=repos)
         else:
-            repos = client.get_repos(user_data)
-        return dict(user_data=user_data, repos=repos)
+            return dict()
+
+    def _get_access_token(self):
+        if self.request.user.is_authenticated:
+            # XXX: We always can to this, cuz users
+            #      are always logged in with github
+            user_auth = UserSocialAuth.objects.get(user=self.request.user)
+            return user_auth.extra_data['access_token']
+        else:
+            return None
